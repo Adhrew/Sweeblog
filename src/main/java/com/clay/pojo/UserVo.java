7f659170455d66c1d9e075965c302b1287d9326e
rainboycc <1306463032@qq.com>
2019-08-22 14:41:20 +0800

    各个模块的模糊查询

--- src/main/java/com/clay/pojo/UserVo.java
+++ src/main/java/com/clay/pojo/UserVo.java
@@ -2,7 +2,14 @@ package com.clay.pojo;
 
 public class UserVo {
 	private Integer user_identity;//根据用户认证状态查询
+	private String user_str_like;//模糊查询
 	
+	public String getUser_str_like() {
+		return user_str_like;
+	}
+	public void setUser_str_like(String user_str_like) {
+		this.user_str_like = user_str_like;
+	}
 	public Integer getUser_identity() {
 		return user_identity;
 	}
